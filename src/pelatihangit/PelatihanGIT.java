ea197dab1192f81741d0dddfb0fa292309c9b545
Insan23 <green.blaze235@gmail.com>
2018-04-06 13:21:29 +0700

    init

---
+++ src/pelatihangit/PelatihanGIT.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pelatihangit;
+
+/**
+ *
+ * @author eldi, aleq, abbi
+ */
+public class PelatihanGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //berhasil clone dan push change
+        
+        buatMethod();
+    }
+    
+    public static void buatMethod() {
+        System.out.println("Hello World!!");
+        System.out.println("hhhhhhhhhhaaaaaaaaaaaaaaaaaaa");
+        System.out.println("testing");
+        System.out.println("git add commit push");
+        
+        System.out.println("");
+    }
+    
+    
+}
